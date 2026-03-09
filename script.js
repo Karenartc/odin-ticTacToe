129d5091eb845b53b53363183aa602070bc28c9c
Karenartc <karenrc.rrss@gmail.com>
2026-03-09 12:14:06 -0300

    feat: implement winner detection in tic tac toe game

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ function GameBoard(){
     const getBoard = () => board;
 
     const placeMark = (index, mark) => {
-        console.log(`Index: ${index}`);
-        console.log(`Mark: ${mark}`);
-
         if(board[index] === ''){
             board[index] = mark;
             return true;
@@ -36,38 +33,69 @@ function GameController(){
     const gameBoard = GameBoard();
 
     let currentPlayer = player1;
-    
+    let isGameOver = false;
+    let winner = null;
+
     const getBoard = () => gameBoard.getBoard();
 
     const playTurn = (index) => {
+        if(!isGameOver){
+             let succes = gameBoard.placeMark(index, currentPlayer.getMark());
+            if(succes) {
+                let isWinner = checkWinner();
+                let isTie = checkTie();
 
-        let succes = gameBoard.placeMark(index, currentPlayer.getMark());
-        if(succes){
-            console.log(succes);
-            currentPlayer = switchPlayer(currentPlayer);
-            
+                if (isWinner){
+                    winner = currentPlayer;
+                    console.log(winner.getName());
+                    isGameOver = true;
+                } else if(isTie){
+                    isGameOver = true;
+                }else{
+                    currentPlayer = switchPlayer(currentPlayer);
+                }
+            };
         }
+        
     }
 
     const switchPlayer = (player) => {
-        if(player === player1){
-            return player2;
-        } else {
-            return player1;
-        }
+        return (player === player1) ? player2 : player1;
     };
 
-    const checkWinner = () => {};
+    const checkWinner = () => {
+        let currentBoard = gameBoard.getBoard();
+        const winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+        for(let i = 0; i < winningMoves.length; i++){
+            let [first, second, third] = winningMoves[i];
+            if(currentBoard[first] !== '' && (currentBoard[first] === currentBoard[second] && currentBoard[second] === currentBoard[third])){
+                console.log([first, second, third]);
+                return true;
+            }             
+        }
+
+        return false;
+    };
+
     const checkTie = () => {};
-    const restart = () => {};
+    
+    const restart = () => {
+        gameBoard.resetGameBoard();
+        currentPlayer = player1;
+        isGameOver = false;
+        winner = null;
+    };
 
     return {getBoard, playTurn, switchPlayer, checkWinner, checkTie, restart};
 }
 
 const game = GameController();
 game.playTurn(0); 
-game.playTurn(4);
-game.playTurn(1); 
 game.playTurn(5);
+game.playTurn(4); 
+game.playTurn(3);
+game.playTurn(7); 
 game.playTurn(2); 
+game.playTurn(8);  
 console.log(game.getBoard());
